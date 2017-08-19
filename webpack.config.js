87a9dd33c85f64897dfb1efcc2036a975f8c8200
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-08-19 16:34:56 +1000

    split vendor libraries to isolated bundled file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
-// const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
+const config = {
   entry: {
     background: './src/background.js',
     popup: './src/popup.jsx',
+    vendor: [
+      'iconv-lite',
+      'material-ui',
+      'prop-types',
+      'react',
+      'react-dom',
+      'styled-components',
+    ],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -24,4 +34,26 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new BundleAnalyzerPlugin(),
+  ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  Object.assign(config, {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new UglifyJSPlugin(),
+    ],
+  });
+}
+
+module.exports = config;
