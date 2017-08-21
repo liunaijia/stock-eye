8328e8a1dd3e310aa884c2712f91192eb1bab5f0
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-08-22 08:36:37 +1000

    extract to styled components

--- src/components/Bids.jsx
+++ src/components/Bids.jsx
@@ -16,6 +16,20 @@ const defaultProps = {
   className: null,
 };
 
+const BidType = styled.span`
+  width: 2rem;
+`;
+
+const Price = styled.span`
+  width: 3rem;
+  text-align: right;
+`;
+
+const Lot = styled.span`
+  flex-grow: 1;
+  text-align: right;
+`;
+
 const Bid = (index, type, bid, currentPrice) => {
   const key = type + index;
 
@@ -25,9 +39,9 @@ const Bid = (index, type, bid, currentPrice) => {
 
   return (
     <li key={key} data-vs-current-price={vsCurrentPrice}>
-      <span>{type === 'buy' ? '买' : '卖'}{index + 1}</span>
-      <span>{bid.price.toLocaleString(undefined, { minimumFractionDigits: 2 })}</span>
-      <span>{Math.round(bid.amount / 100)}</span>
+      <BidType>{type === 'buy' ? '买' : '卖'}{index + 1}</BidType>
+      <Price>{bid.price.toLocaleString(undefined, { minimumFractionDigits: 2 })}</Price>
+      <Lot>{Math.round(bid.amount / 100)}</Lot>
     </li>
   );
 };
@@ -53,22 +67,6 @@ export default styled(Bids)`
   li {
     display: flex;
     line-height: 1rem;
-
-    span {
-      &:nth-child(1) {
-        width: 2rem;
-      }
-
-      &:nth-child(2) {
-        width: 3rem;
-        text-align: right;
-      }
-
-      &:nth-child(3) {
-        flex-grow: 1;
-        text-align: right;
-      }
-    }
 
     &[data-vs-current-price=higher] {
       span:nth-child(2) {
