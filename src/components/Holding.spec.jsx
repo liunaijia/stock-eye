87c1658b5db62f5164ad765e6fc85a6309df1c16
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-08-26 23:07:19 +1000

    add test for Holding component

---
+++ src/components/Holding.spec.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Holding from './Holding';
+
+describe('<Holding />', () => {
+  let component = null;
+
+  beforeAll(() => {
+    component = mount(
+      <table>
+        <tbody>
+          <Holding stockName="中国银行" stockAmount={1000} sellableAmount={500} floating={23.41} floatingRate="1.92%" />
+        </tbody>
+      </table>,
+    );
+  });
+
+  it('renders stock name', () => {
+    expect(component.find('td').at(0).text()).toBe('中国银行');
+  });
+
+  it('renders stock amount', () => {
+    expect(component.find('td').at(1).text()).toBe('1000');
+  });
+
+  it('renders sellable amount', () => {
+    expect(component.find('td').at(2).text()).toBe('500');
+  });
+
+  it('renders floating information', () => {
+    expect(component.find('td').at(3).text()).toBe('23.41 (1.92%)');
+  });
+});
