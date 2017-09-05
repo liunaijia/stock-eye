99f06efa29773505f34600abe48f0daf60fa5ce7
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-09-05 11:11:57 +1000

    add tests

--- src/jobs/job.spec.js
+++ src/jobs/job.spec.js
@@ -1,51 +1,72 @@
-import * as time from '../time';
-import { runDuringTradeTime } from './job';
-
+/* eslint-disable global-require */
 describe('runDuringTradeTime', () => {
   let funcToRun;
 
   beforeEach(() => {
+    jest.resetModules();
     funcToRun = jest.fn();
   });
 
-  it('calls function to run when it is trade time', () => {
-    time.isTradeTime = jest.fn().mockReturnValue(true);
+  function setIsTradeTime(mockValue) {
+    jest.doMock('../time', () => ({
+      isTradeTime: jest.fn().mockReturnValue(mockValue),
+    }));
+  }
 
-    runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
+  it('calls function to run when it is trade time', () => {
+    setIsTradeTime(true);
+
+    const job = require('./job');
+    job.runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
 
     expect(funcToRun).toHaveBeenCalled();
   });
 
   it('does not call function to run when it is not trade time', () => {
-    time.isTradeTime = jest.fn().mockReturnValue(false);
+    setIsTradeTime(false);
 
-    runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
+    const job = require('./job');
+    job.runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
 
     expect(funcToRun).not.toHaveBeenCalled();
   });
 
   it('calls function to run when run on start up is enabled even it is not trade time', () => {
-    time.isTradeTime = jest.fn().mockReturnValue(false);
+    setIsTradeTime(false);
 
-    runDuringTradeTime({ interval: 3, runOnStartUp: true })(funcToRun);
+    const job = require('./job');
+    job.runDuringTradeTime({ interval: 3, runOnStartUp: true })(funcToRun);
 
     expect(funcToRun).toHaveBeenCalled();
   });
 
+  fit('x', () => {
+    jest.useFakeTimers();
+    const time = require('../time');
+    time.sleep(3);
+
+    expect(setTimeout.mock.calls.length).toBe(1);
+    expect(setTimeout.mock.calls[0][1]).toBe(3000);
+
+    time.sleep(1);
+    expect(setTimeout.mock.calls.length).toBe(2);
+    expect(setTimeout.mock.calls[1][1]).toBe(1000);
+  });
+
   it('sleeps with given intervals', () => {
     jest.useFakeTimers();
+    setIsTradeTime(false);
 
-    // if it is trade time, timeout will not be triggered which might be caused by async funcToRun.
-    time.isTradeTime = jest.fn().mockReturnValue(false);
-
-    runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
+    const job = require('./job');
+    job.runDuringTradeTime({ interval: 3, runOnStartUp: false })(funcToRun);
 
     expect(setTimeout.mock.calls.length).toBe(1);
     expect(setTimeout.mock.calls[0][1]).toBe(3000);
     jest.runOnlyPendingTimers();
 
     // Regardless how to setup the context, the timeout will not be trigger for another time.
-    // expect(setTimeout.mock.calls.length).toBe(2);
-    // expect(setTimeout.mock.calls[1][1]).toBe(3000);
+    expect(setTimeout.mock.calls.length).toBe(2);
+    expect(setTimeout.mock.calls[1][1]).toBe(3000);
   });
 });
+/* eslint-enable global-require */
--- src/jobs/portfolioJob.spec.js
+++ src/jobs/portfolioJob.spec.js
@@ -1,41 +1,61 @@
-import * as newoneApi from '../newoneApi';
-import * as job from './job';
-import { getPortfolio } from './portfolioJob';
+/* eslint-disable global-require */
+describe('portfolioJob', () => {
+  let newoneApi;
+  let portfolio;
 
-describe('getPortfolio', async () => {
-  fit('calls api to get portfolio in trade time', async () => {
-    // jest.useFakeTimers();
+  beforeEach(() => {
+    jest.resetModules();
+    newoneApi = require('../newoneApi');
 
-    const reloadPortfolio = jest.fn();
-    job.runDuringTradeTime = jest.fn().mockImplementation(
-      () => reloadPortfolio,
-    );
-
-    // const portfolio = {};
-    // newoneApi.getPortfolio = jest.fn().mockImplementation(() => Promise.resolve(portfolio));
-
-    expect(reloadPortfolio).toBeCalled();
+    portfolio = {};
+    newoneApi.getPortfolio = jest.fn().mockReturnValue(portfolio);
   });
 
-  it('returns portfolio from the api', async () => {
-    const portfolio = {};
-    newoneApi.getPortfolio = jest.fn().mockImplementation(() => Promise.resolve(portfolio));
+  function setIsTradeTime(mockValue) {
+    jest.doMock('../time', () => ({
+      isTradeTime: jest.fn().mockReturnValue(mockValue),
+    }));
+  }
 
-    time.isTradeTime = jest.fn().mockReturnValue(false);
+  describe('import portfolioJob', () => {
+    it('calls the api to get portfolio in trade time', () => {
+      setIsTradeTime(true);
 
-    const result = await getPortfolio();
+      require('./portfolioJob');
 
-    expect(newoneApi.getPortfolio).toBeCalled();
-    expect(result).toBe(portfolio);
+      expect(newoneApi.getPortfolio).toBeCalled();
+    });
+
+    it('does not call the api to get portfolio when out of trade time', () => {
+      setIsTradeTime(false);
+
+      require('./portfolioJob');
+
+      expect(newoneApi.getPortfolio).not.toBeCalled();
+    });
   });
 
-  it('caches the portfolio from the api', async () => {
-    const portfolio = {};
-    newoneApi.getPortfolio = jest.fn().mockImplementation(() => Promise.resolve(portfolio));
+  describe('getPortfolio', () => {
+    it('returns portfolio from the api', async () => {
+      setIsTradeTime(false);
+      const portfolioJob = require('./portfolioJob');
 
-    const result = await getPortfolio();
+      const result = await portfolioJob.getPortfolio();
 
-    expect(newoneApi.getPortfolio).toBeCalled();
-    expect(result).toBe(portfolio);
+      expect(newoneApi.getPortfolio).toBeCalled();
+      expect(result).toBe(portfolio);
+    });
+
+    it('caches the portfolio from the api', async () => {
+      setIsTradeTime(false);
+      const portfolioJob = require('./portfolioJob');
+
+      await portfolioJob.getPortfolio();
+      expect(newoneApi.getPortfolio).toHaveBeenCalledTimes(1);
+
+      await portfolioJob.getPortfolio();
+      expect(newoneApi.getPortfolio).toHaveBeenCalledTimes(1);
+    });
   });
 });
+/* eslint-enable global-require */
