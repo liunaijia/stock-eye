e56e3ffe608a79e8a7927a7646b1c5888da28517
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-10-10 15:10:42 +1000

    fix stylelint errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-styled-components',
+  ],
   syntax: 'scss',
   processors: ["stylelint-processor-styled-components"],
 };
