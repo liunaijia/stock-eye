10d9655a21914496038db6ee1bc8612f3c5ede33
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-01-05 18:25:27 +1000

    use 2 columns layout for TradeSuggesion component

--- src/components/GroupTradeSuggestions.jsx
+++ src/components/GroupTradeSuggestions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { string, shape, func } from 'prop-types';
-import { Card } from 'antd';
+import { Card, Col, Row } from 'antd';
 import styled from 'styled-components';
 
 import StockTradeSuggestion from './StockTradeSuggestion';
@@ -12,41 +12,47 @@ const CardTitle = styled.span`
 const GroupTradeSuggestions = ({
   className, groupName, buyingGap, sellingGap, onPlaceOrder,
 }) => (
-  <Card className={className} title={groupName}>
-    {buyingGap &&
-    <Card
-      type="inner"
-      title={<CardTitle tradeType="buy">买入{buyingGap.toBuy.stockName}{buyingGap.toBuy.price}</CardTitle>}
-    >
-      <p>
+  <Card className={className} title={groupName} styles={{}}>
+    <Row gutter={16}>
+      <Col span={12}>
+        {buyingGap &&
+        <Card
+          type="inner"
+          title={<CardTitle tradeType="buy">买入{buyingGap.toBuy.stockName}{buyingGap.toBuy.price}</CardTitle>}
+        >
+          <p>
         GAP：[{buyingGap.value}]
         相比：{`${buyingGap.compareWith.stockName} ${buyingGap.compareWith.price}`}
-      </p>
-      <time>{new Date(buyingGap.timestamp).toLocaleTimeString()}</time>
-      <StockTradeSuggestion
-        tradeType="buy"
-        {...buyingGap.toBuy}
-        onPlaceOrder={onPlaceOrder}
-      />
-    </Card>
-    }
-    {sellingGap &&
-    <Card
-      type="inner"
-      title={<CardTitle tradeType="sell">卖出{sellingGap.toSell.stockName}{sellingGap.toSell.price}</CardTitle>}
-    >
-      <p>
+          </p>
+          <time>{new Date(buyingGap.timestamp).toLocaleTimeString()}</time>
+          <StockTradeSuggestion
+            tradeType="buy"
+            {...buyingGap.toBuy}
+            onPlaceOrder={onPlaceOrder}
+          />
+        </Card>
+      }
+      </Col>
+      <Col span={12}>
+        {sellingGap &&
+        <Card
+          type="inner"
+          title={<CardTitle tradeType="sell">卖出{sellingGap.toSell.stockName}{sellingGap.toSell.price}</CardTitle>}
+        >
+          <p>
         GAP：[{sellingGap.value}]
         相比：{`${sellingGap.compareWith.stockName} ${sellingGap.compareWith.price}`}
-      </p>
-      <time>{new Date(sellingGap.timestamp).toLocaleTimeString()}</time>
-      <StockTradeSuggestion
-        tradeType="sell"
-        {...sellingGap.toSell}
-        onPlaceOrder={onPlaceOrder}
-      />
-    </Card>
-    }
+          </p>
+          <time>{new Date(sellingGap.timestamp).toLocaleTimeString()}</time>
+          <StockTradeSuggestion
+            tradeType="sell"
+            {...sellingGap.toSell}
+            onPlaceOrder={onPlaceOrder}
+          />
+        </Card>
+      }
+      </Col>
+    </Row>
   </Card>
 );
 
