aa7b811cee8b3183eefad729918e2bad66a988b5
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-01-25 20:22:07 +1000

    upgrade to webpack 4 and webpack dev server 3

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
+  mode: 'development',
   entry: {
     background: './src/background.js',
     popup: './src/App.jsx',
@@ -10,6 +11,7 @@ const config = {
   devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist',
+    // headers 实现CROS
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -33,15 +35,14 @@ const config = {
       },
     ],
   },
+  optimization: {
+    // CommonsChunkPlugin is removed, see https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   plugins: [
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: m => m.context && m.context.includes('node_modules'),
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime',
-    }),
     // new webpack.optimize.ModuleConcatenationPlugin(),
     // new WebpackMonitor({
     //   capture: true, // -> default 'true'
