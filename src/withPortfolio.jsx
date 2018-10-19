72b69f31165037d6c8160405f656b7441213d4c6
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-10-19 12:52:55 +1000

    stop checking portfolio

--- src/withPortfolio.jsx
+++ src/withPortfolio.jsx
@@ -5,11 +5,12 @@ import { runDuringTradeTime } from './jobs/job';
 function withPortfolio(WrappedComponent) {
   return class extends Component {
     componentDidMount() {
-      // runDuringTradeTime({ interval: 10, runOnStartUp: true })(async () => {
-      //   this.setState({ ...this.state, isLoading: true });
-      //   const portfolio = await getPortfolioFromApi();
-      //   this.setState({ ...portfolio, isLoading: false });
-      // });
+      runDuringTradeTime({ interval: 10, runOnStartUp: true })(async () => {
+        this.setState({ ...this.state, isLoading: true });
+        //   const portfolio = await getPortfolioFromApi();
+        const portfolio = {};
+        this.setState({ ...portfolio, isLoading: false });
+      });
     }
 
     render() {
