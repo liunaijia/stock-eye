640aa61bda67a156cfb656da7135ee60402bafb7
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-11-01 22:07:21 +1000

    watch current quotes

--- src/components/QuotesFetcher.jsx
+++ src/components/QuotesFetcher.jsx
@@ -4,6 +4,7 @@ import {
   any, arrayOf, shape, func,
 } from 'prop-types';
 import store from '../store';
+import { runDuringTradeTime } from '../jobs/job';
 
 class QuotesFetcher extends Component {
   static propTypes = {
@@ -11,21 +12,34 @@ class QuotesFetcher extends Component {
       stocks: any,
       lookBackDays: any,
     })),
+    stockCodes: arrayOf(any),
     fetchHistoryQuotes: func.isRequired,
+    fetchCurrentQuotes: func.isRequired,
   }
 
   static defaultProps = {
     groups: [],
+    stockCodes: [],
   }
 
-  componentWillMount() {
+  componentDidMount() {
+    // init fetch
     this.fetchHistoryQuotes();
+    this.watchCurrentQuotes();
   }
 
-  componentDidUpdate({ groups: prevGroups }) {
-    const { groups } = this.props;
+  componentDidUpdate(prevProps) {
+    const { groups, stockCodes } = this.props;
+    const { groups: prevGroups, stockCodes: prevStockCodes } = prevProps;
+
     if (groups !== prevGroups) {
+      // when groups change fetch history quotes again
       this.fetchHistoryQuotes();
+    }
+
+    if (stockCodes !== prevStockCodes) {
+      // when stock codes change fetch current quotes again
+      this.watchCurrentQuotes();
     }
   }
 
@@ -37,17 +51,40 @@ class QuotesFetcher extends Component {
     });
   }
 
+  watchCurrentQuotes() {
+    const { stockCodes, fetchCurrentQuotes } = this.props;
+
+    // cancel previous watching job
+    if (this.cancelJob) {
+      this.cancelJob();
+    }
+    this.cancelJob = runDuringTradeTime({
+      interval: 3, runOnStartUp: true,
+    })(() => {
+      console.log('fetchCurrentQuotes', stockCodes);
+      if (stockCodes.length) {
+        fetchCurrentQuotes({ stockCodes });
+      }
+    });
+  }
+
   render() {
     return null;
   }
 }
 
 const mapState = state => ({
-  groups: store.select.groups.all(state),
+  groups: store.select.groups.allGroups(state),
+  stockCodes: store.select.groups.allStockCodes(state),
+  // some: store.select.historyQuotes.get(state)('sh601328', new Date('2018-10-30')),
 });
 
-const mapDispatch = ({ historyQuotes: { fetch } }) => ({
-  fetchHistoryQuotes: fetch,
+const mapDispatch = ({
+  historyQuotes: { fetch: fetchHistoryQuotes },
+  currentQuotes: { fetch: fetchCurrentQuotes },
+}) => ({
+  fetchHistoryQuotes,
+  fetchCurrentQuotes,
 });
 
 export default connect(mapState, mapDispatch)(QuotesFetcher);
