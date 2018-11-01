1bbcd1f593df1e8958420d032fad912588088bce
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-11-01 22:10:35 +1000

    remove redux connection from WithQuotes

--- src/components/hq/WithQuotes.jsx
+++ src/components/hq/WithQuotes.jsx
@@ -1,23 +1,18 @@
 import { Component } from 'react';
 import {
-  arrayOf, string, number, func, any,
+  arrayOf, string, number, func,
 } from 'prop-types';
-import { connect } from 'react-redux';
 import { fetchStocks } from '../../stockData';
 import { runDuringTradeTime } from '../../jobs/job';
 
 class WithQuotes extends Component {
   static propTypes = {
-    lookBackDaysOfStocks: any,
-    fetchQuotes: func.isRequired,
-
     stockCodes: arrayOf(string),
     lookBackDays: number,
     children: func.isRequired,
   }
 
   static defaultProps = {
-    lookBackDaysOfStocks: {},
     stockCodes: null,
     lookBackDays: 1,
   }
@@ -26,14 +21,11 @@ class WithQuotes extends Component {
     quotes: null,
   }
 
-  componentDidUpdate() {
+  async componentDidMount() {
     runDuringTradeTime({ interval: 3, runOnStartUp: true })(async () => {
       const {
-        stockCodes, lookBackDays, fetchQuotes, lookBackDaysOfStocks,
+        stockCodes, lookBackDays,
       } = this.props;
-      // lookBackDaysOfStocks
-      // const quotes = await fetchQuotes();
-      // console.log(quotes);
       const quotes = await fetchStocks(stockCodes, lookBackDays);
       // console.log('QuotesContainer is fetching stock data');
       this.setState({ quotes });
@@ -47,11 +39,5 @@ class WithQuotes extends Component {
   }
 }
 
-const mapState = (state, ownProps) => ({
-});
 
-const mapDispatch = ({ quotes: { fetch } }, ownProps) => ({
-  fetchQuotes: fetch,
-});
-
-export default connect(mapState, mapDispatch)(WithQuotes);
+export default WithQuotes;
