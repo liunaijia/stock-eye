a2099d26c181f8a01f63ba7dbe1248a89165a3db
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-11-01 22:06:09 +1000

    return groups selector

--- src/components/hq/LiteHqContainer.js
+++ src/components/hq/LiteHqContainer.js
@@ -3,7 +3,7 @@ import store from '../../store';
 import LiteHq from './LiteHq';
 
 const mapState = state => ({
-  stockCodes: store.select.groups.stockCodes(state),
+  stockCodes: store.select.groups.allStockCodes(state),
 });
 
 export default connect(mapState)(LiteHq);
--- src/models/groups.js
+++ src/models/groups.js
@@ -2,12 +2,14 @@ export default {
   state: {
     // [id]: { ... }
   },
+
   reducers: {
     // handle state changes with pure functions
     add(state, payload) {
       return { ...state, [payload.id]: payload };
     },
   },
+
   effects: dispatch => ({
     // watch quotes for all stocks in all groups
     async watchQuotes(payload, rootState) {
@@ -15,18 +17,19 @@ export default {
       dispatch.groups.add(payload);
     },
   }),
+
   selectors: (slice, createSelector, hasProps) => ({
-    all() {
+    allGroups() {
       // return all groups as an array
       return slice(
         groups => Object.values(groups),
       );
     },
-    stockCodes() {
+    allStockCodes() {
       // return all stock codes from all groups
-      return slice(
-        groups => Object.values(groups)
-          .reduce((all, group) => all.concat(group.stocks), []),
+      return createSelector(
+        this.allGroups,
+        groups => groups.reduce((all, group) => all.concat(group.stocks), []),
       );
     },
   }),
