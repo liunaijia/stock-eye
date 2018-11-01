60b4cc66f10639a82fc645b380f9db4ce9ab5bb7
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-11-01 22:05:05 +1000

    make the job cancellable

--- src/jobs/job.js
+++ src/jobs/job.js
@@ -4,23 +4,37 @@ import { sendNotification } from '../notification';
 // eslint-disable-next-line import/prefer-default-export
 export const runDuringTradeTime = ({ interval = 1, runOnStartUp = true }) => {
   let hasStarted = false;
-  const func = async (block) => {
-    try {
-      if ((runOnStartUp && !hasStarted) || isTradeTime()) {
-        hasStarted = true;
-        await block();
-      }
+  let cancelled = false;
+
+  function schedule(block) {
+    // if cancelled flag is set, stop running
+    if (cancelled) {
+      return null;
+    }
+
+    // wrap the block into an async function, because the return value of schedule function
+    // needs to be a function not a promise.
+    (async () => {
+      try {
+        if ((runOnStartUp && !hasStarted) || isTradeTime()) {
+          hasStarted = true;
+          await block();
+        }
       // else {
       // setBadge('');
       // }
-    } catch (e) {
-      console.error(e);
-      sendNotification({ title: e.message });
-    } finally {
-      await sleep(interval);
-      func(block);
-    }
-  };
+      } catch (e) {
+        console.error(e);
+        sendNotification({ title: e.message });
+      } finally {
+        await sleep(interval);
+        schedule(block);
+      }
+    })();
 
-  return func;
+    const cancel = () => { cancelled = true; };
+    return cancel;
+  }
+
+  return schedule;
 };
