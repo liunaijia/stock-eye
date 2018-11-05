470e3fb1f772c4d6bf9eff7bd56ecc816f17694c
Naijia Liu <naijialiu@geniesolutions.com.au>
2018-11-05 15:34:20 +1000

    make jest transpile lodash-es code

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   setupFiles: ['<rootDir>/testShim.js', '<rootDir>/testSetup.js'],
+  // white list lodash-es
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es/.*)'],
 };
