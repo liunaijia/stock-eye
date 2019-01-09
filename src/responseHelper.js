d53e97e9b33eb4195f57ed2f82a1920e03e93e97
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-01-09 14:22:52 +1000

    re-decode html with charset from meta

--- src/responseHelper.js
+++ src/responseHelper.js
@@ -1,16 +1,17 @@
 import { decode } from 'iconv-lite';
 
-export const readContentType = (response = new Response()) => {
-  const contentType = response.headers.get('Content-Type');
+const parseContentType = (contentType) => {
   const groups = contentType.match(/([^;]+)(;\s*charset=([^;]+))?(;\s*boundary=([^;]+))?/);
   return { mimeType: groups[1], charset: groups[3], boundary: groups[5] };
 };
 
-export const readAsText = async (response = new Response()) => {
-  const { charset } = readContentType(response);
-  if (charset) {
+export const readContentType = (response = new Response()) => parseContentType(response.headers.get('Content-Type'));
+
+export const readAsText = async (response = new Response(), charset = null) => {
+  const withCharset = charset || readContentType(response).charset;
+  if (withCharset) {
     const bytes = await response.arrayBuffer();
-    const text = decode(Buffer.from(bytes), charset);
+    const text = decode(Buffer.from(bytes), withCharset);
     return text;
   }
   return response.text();
@@ -29,9 +30,19 @@ export const readAsDom = async (response = new Response()) => {
     return null;
   }
 
-  const text = await readAsText(response);
-  const { mimeType } = readContentType(response);
-
+  const text = await readAsText(response.clone());
+  const { mimeType, charset } = readContentType(response);
   const dom = new DOMParser().parseFromString(text, mimeType);
+
+  const metaNode = dom.querySelector('meta[http-equiv="Content-Type"]');
+  if (metaNode) {
+    const { charset: htmlCharset } = parseContentType(metaNode.getAttribute('content'));
+    // re-decode the content if the charset defined in meta is different with the one in header
+    if (htmlCharset && htmlCharset !== charset) {
+      const decodedText = await readAsText(response.clone(), htmlCharset);
+      return new DOMParser().parseFromString(decodedText, mimeType);
+    }
+  }
+
   return dom;
 };
