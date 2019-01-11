71b7dc8b5b25ce458c9d3953168f9c2e50aa225c
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-01-11 21:31:06 +1000

    show current ratio of selected stock in title

--- src/components/hq/Hq.jsx
+++ src/components/hq/Hq.jsx
@@ -1,11 +1,21 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { instanceOf } from 'prop-types';
 import { Card } from 'antd';
 import Quotes from './Quotes';
 
 const Hq = ({ quotes }) => {
   const [stockCodeInWatch, setStockCodeInWatch] = useState();
+
+  useEffect(() => {
+    if (stockCodeInWatch) {
+      const allQuotes = Object.values(quotes).reduce((a, b) => a.concat(b), []);
+      const stockInWatch = allQuotes.find(({ stockCode }) => stockCode === stockCodeInWatch);
+      if (stockInWatch) {
+        document.title = `${stockInWatch.name.substr(0, 1)} ${stockInWatch.currentRatio.toFixed(2)}%`;
+      }
+    }
+  });
 
   const handleWatch = (event) => {
     setStockCodeInWatch(event.target.value);
