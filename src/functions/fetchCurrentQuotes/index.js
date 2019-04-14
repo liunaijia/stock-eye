2c48f1fb7ef3d54124e5c198ead1284d4349e1d9
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-04-14 22:39:11 +1000

    add fetch current quotes lambda function

--- src/functions/fetchCurrentQuotes/index.js
+++ src/functions/fetchCurrentQuotes/index.js
@@ -1,43 +1,72 @@
 import https from 'https';
-import util from 'util';
 
-const get = util.promisify(https.get);
+const get = (...args) => new Promise((resolve, reject) => {
+  https.get(...args, (response) => {
+    const chunks = [];
+    response.on('data', chunk => chunks.push(chunk));
+    response.on('end', () => resolve({ ...response, data: chunks.join('') }));
+  }).on('error', reject);
+});
+
+
+const getValueFrom = (array = [], index = 0) => parseFloat(array[index]);
+
+const getValuesFrom = (array = [], index = 0, length = 1) => Array.from({
+  length,
+}, (_, i) => getValueFrom(array, index + i));
+
+const collapseArray = (array = []) => array.reduce((acc, _, idx) => {
+  if (idx % 2 === 0) {
+    acc.push({
+      price: array[idx + 1],
+      amount: array[idx],
+    });
+  }
+  return acc;
+}, []);
+
+const parse = (text = '') => text
+  .split(';')
+  .slice(0, -1)
+  .map((line) => {
+    const [, variable = '', valueExp = ''] = /(.*?)="(.*?)"/.exec(line);
+    return [variable.substr(variable.lastIndexOf('_') + 1), valueExp.split(',')];
+  })
+  .map((item) => {
+    const [stockCode, rawValues] = item;
+    const stock = {
+      stockCode,
+      name: rawValues[0],
+      openAt: getValueFrom(rawValues, 1),
+      closeAt: getValueFrom(rawValues, 2),
+      current: getValueFrom(rawValues, 3),
+      buyingAt: getValueFrom(rawValues, 6), // 竞买价，即“买一“报价
+      sellingAt: getValueFrom(rawValues, 7), // 竞卖价，即“卖一“报价
+      buyingBids: collapseArray(getValuesFrom(rawValues, 10, 10)),
+      sellingBids: collapseArray(getValuesFrom(rawValues, 20, 10)),
+      timestamp: new Date(`${rawValues[30]} ${rawValues[31]}`).toISOString(),
+    };
+    return stock;
+  });
 
 export const handler = async (event, context, callback) => {
   try {
-    // const stockCodes = ['sz000001'];
-    // try {
-    //   const response = await get(`https://hq.sinajs.cn/rn=${new Date().getTime()}&list=${stockCodes.join(',')}`);
-    //   console.log(2);
-    //   return response;
-    // } catch (e) {
-    //   console.log(e.message);
-    // }
+    const { stockCodes } = event.queryStringParameters;
+    const response = await get(`https://hq.sinajs.cn/rn=${new Date().getTime()}&list=${stockCodes}`);
+    const result = parse(response.data);
 
     callback(null, {
       statusCode: 200,
-      body: event.body,
-      // JSON.stringify({
-      //   RideId: rideId,
-      //   Unicorn: unicorn,
-      //   UnicornName: unicorn.Name,
-      //   Eta: '30 seconds',
-      //   Rider: username,
-      // }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
+      body: JSON.stringify(result),
     });
   } catch (error) {
+    console.error(error);
     callback(null, {
       statusCode: 500,
       body: JSON.stringify({
-        Error: error.message,
+        Error: error.message || error,
         Reference: context.awsRequestId,
       }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
     });
   }
 };
