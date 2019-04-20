248cc769662ebfe916d48ec33b410d2d58fe5925
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-04-20 20:16:05 +1000

    rename webpack.config of website

--- webpack-base.config.js
+++ webpack-base.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies, @typescript-eslint/no-var-requires */
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { RESOLVE_EXTENSIONS } = require('./const');
 
--- webpack-web.config.js
+++ webpack-web.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
