c5c106592c83e1d3a092aaf9d0cf2e9139d9bd76
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-09-09 09:24:51 +1000

    add react dom hot loader

--- webpack-web.config.js
+++ webpack-web.config.js
@@ -4,24 +4,38 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const createConfig = require('./webpack-base.config');
 
-module.exports = createConfig({
-  entry: './src/website',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: '[name].js',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+module.exports = createConfig((env, argv) => {
+  const config = {
+    entry: './src/website',
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      filename: '[name].js',
     },
-    runtimeChunk: true,
-  },
-  plugins: [
-    new webpack.HashedModuleIdsPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/website/index.html',
-    }),
-    // enable hot module replacement
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      runtimeChunk: true,
+    },
+    plugins: [
+      new webpack.HashedModuleIdsPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/website/index.html',
+      }),
+      // enable hot module replacement
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  };
+
+  if (argv.mode === 'development') {
+    Object.assign(config, {
+      resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+      },
+    });
+  }
+
+  return config;
 });
