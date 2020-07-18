ea63bb5973c544dd515574187feccd63fb809896
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-07-18 14:06:53 +1000

    fix jest running environment

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   setupFiles: [
-    '<rootDir>/src/testShim.js',
     '<rootDir>/src/testSetup.js',
   ],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
   coverageReporters: ['lcov'],
 };
